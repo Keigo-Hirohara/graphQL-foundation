a2f30d3b44dc0c40bab57d0267861a3a4c162fe2
keigo-h <keigo-h@weseek.co.jp>
2022-09-19 12:41:20 +0900

    make foreign key to UserType

--- schema/schema.ts
+++ schema/schema.ts
@@ -4,13 +4,21 @@ import {
   GraphQLString,
   GraphQLSchema,
 } from 'graphql';
-import { resolve } from 'path';
-const _ = require('lodash');
+import axios from 'axios';
 
-const users = [
-  { id: '23', firstName: 'Mickey', age: 93 },
-  { id: '47', firstName: 'Donuld', age: 88 },
-];
+// const users = [
+//   { id: '23', firstName: 'Mickey', age: 93 },
+//   { id: '47', firstName: 'Donuld', age: 88 },
+// ];
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -18,6 +26,15 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
@@ -28,7 +45,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
       },
     },
   },
